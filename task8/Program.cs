8e65867adac4bc5dd872d2abb7eca9b048ab3fcf
Igor <test@mail.ru>
2023-06-10 15:54:20 +0300

    Добавил решение восьмой задачи (task8)

---
+++ task8/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+    // 5 -> 2, 4
+    // 8 -> 2, 4, 6, 8
+// *Число "0" является чётным ))
+Console.WriteLine("Чётные числа от 1 до N");
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0)
+{
+    number = -number;
+    string znak = "-";
+    System.Console.WriteLine($"Вы ввели отрицательное число: {znak}{number}. Результат программы выведет значения по модулю"); 
+}
+    int i = 0;
+    while (i < number)
+    {
+        if(i%2 == 0)
+        {
+            System.Console.Write($"{i} "); // Вывод чётных чисел.
+        }
+        i++;
+       
+    }
